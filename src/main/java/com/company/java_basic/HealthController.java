f133655d33484d3dd425b5050c9a9378aaf4fa82
htmluiv <1chrhddb@gmail.com>
2025-12-29 16:48:15 +0900

    healthcontroller edit

--- src/main/java/com/company/java_basic/HealthController.java
+++ src/main/java/com/company/java_basic/HealthController.java
@@ -1,13 +1,16 @@
 package com.company.java_basic;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HealthController {
 
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    @ResponseBody
+    public String home() {
+        return "ok";
     }
 }
